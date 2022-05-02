85860ab18e7a529c4326a84bf638d22ffc3de04f
Bill Barnes <billba@microsoft.com>
2022-05-02 13:58:05 -0700

    pretty card names

--- src/game.test.ts
+++ src/game.test.ts
@@ -6,14 +6,48 @@ import {
   cardSuite,
   cardColor,
   cardDeck,
+  cardName,
 } from './game';
 
-describe('game fundamentals', () => {
-  test('newCard', () => {
+describe('newCard', () => {
+  test('4 of hearts, default deck', () => {
     const card = newCard(4, Suite.Hearts);
     expect(cardRank(card)).toBe(4);
     expect(cardSuite(card)).toBe(Suite.Hearts);
     expect(cardColor(card)).toBe(Color.Red);
     expect(cardDeck(card)).toBe(0);
+  });
+
+  test('10 of clubs, specify deck', () => {
+    const card = newCard(10, Suite.Clubs, 3);
+    expect(cardRank(card)).toBe(10);
+    expect(cardSuite(card)).toBe(Suite.Clubs);
+    expect(cardColor(card)).toBe(Color.Black);
+    expect(cardDeck(card)).toBe(3);
+  });
+
+  test('4 of hearts via emoji, default deck', () => {
+    const card = newCard('♥️4');
+    expect(cardRank(card)).toBe(4);
+    expect(cardSuite(card)).toBe(Suite.Hearts);
+    expect(cardColor(card)).toBe(Color.Red);
+    expect(cardDeck(card)).toBe(0);
+  });
+
+  test('Jack of clubs via emoji, specify deck', () => {
+    const card = newCard('♣︎10', 3);
+    expect(cardRank(card)).toBe(10);
+    expect(cardSuite(card)).toBe(Suite.Clubs);
+    expect(cardColor(card)).toBe(Color.Black);
+    expect(cardDeck(card)).toBe(3);
+  });
+});
+
+describe('cardName', () => {
+  test('4 of hearts', () => {
+    expect(cardName(newCard('♥️4'))).toBe('♥️4');
+  });
+  test('Jack of clubs', () => {
+    expect(cardName(newCard('♣︎10'))).toBe('♣︎10');
   });
 });
--- src/game.ts
+++ src/game.ts
@@ -1,7 +1,8 @@
 // A card has a rank, a suite (which includes color info), and a deck.
-// We could use an object, but for a little efficiency we bit pack these into 9 bits - 4 bits for rank (0-12), 2 for suite, and 3 for deck.
+// We could use an object, but for a little efficiency we bit pack these into 9 bits - 4 bits for rank (1-13), 2 for suite, and 3 for deck.
 // It could be a lovely as-god-intended byte if we limit to 4 players, but there's not really a need for that, except aesthetic & religious purity.
 // We could just number cards 0-51 but then we have to use division (instead of bit operations) to get suites and color, and I cannot abide that
+// We could use 0-12 for rank, but it costs us nothing to have a little readability
 
 export type Card = number;
 
@@ -22,8 +23,36 @@ export enum Color {
   Red,
 }
 
-export const newCard = (rank: number, suite: Suite, deck = 0) =>
-  rank | (suite << 4) | (deck << 6);
+export const suitemoji = ['♣︎', '♦️', '♠️', '♥️'];
+export const rankmoji = [
+  'A',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '10',
+  'J',
+  'Q',
+  'K',
+];
+
+export function newCard(name: string, deck?: number): Card;
+export function newCard(rank: number, suite: number, deck?: number): Card;
+export function newCard(...args: [number | string, number?, number?]): Card {
+  return typeof args[0] === 'number'
+    ? args[0] | (args[1]! << 4) | ((args[2]! ?? 0) << 6)
+    : (rankmoji.indexOf((args[0] as string).slice(2)) + 1) |
+        (suitemoji.indexOf((args[0] as string).slice(0, 2)) << 4) |
+        ((args[1]! ?? 0) << 6);
+}
+
+export function cardName(card: Card): string {
+  return `${suitemoji[cardSuite(card)]}${rankmoji[cardRank(card) - 1]}`;
+}
 
 export type Pile = Card[];
 
